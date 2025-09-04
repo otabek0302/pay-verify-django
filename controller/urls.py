e9127b8ad51242ad92b3a6065866d9474edb4dc2
Otabek <otabekjon2002@mail.ru>
2025-09-04 20:44:46 +0500

    Fix static files serving and remove duplicates
    - Add static file serving configuration to urls.py
    - Remove duplicate appointment_detail.css file
    - This should resolve 404 errors for CSS/JS files
    - Static files should now load properly in the browser

--- controller/urls.py
+++ controller/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", RedirectView.as_view(url="/medical_access/", permanent=False)),
     path("admin/", admin.site.urls),
     path("medical_access/", include("medical_access.urls")),
 ]
+
+# Serve static files in development and production
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
