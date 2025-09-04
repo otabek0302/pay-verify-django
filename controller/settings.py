600e1b88240357606b50ff693deca190da6205ee
Otabek <otabekjon2002@mail.ru>
2025-09-04 20:36:44 +0500

    Fix ALLOWED_HOSTS for Docker containerization
    - Add 0.0.0.0 and * to ALLOWED_HOSTS for Docker compatibility
    - This resolves the DisallowedHost error when accessing the app via Docker
    - Allows the application to accept requests from any host in containerized environment

--- controller/settings.py
+++ controller/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-hbyaqg!x@ykw_pb7!cwil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,*').split(',')
 
 # Hikvision Terminal Settings
 HIK_VISITOR_EMPLOYEE_NO = os.environ.get('HIK_VISITOR_EMPLOYEE_NO', 'VISITOR')
